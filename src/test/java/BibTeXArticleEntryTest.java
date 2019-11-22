6cff544e97127f829c7d28d7745511c4a28fdfc8
lututui <lututuibr@gmail.com>
2019-11-22 09:48:01 -0300

    Teste de resultado de BibTeXEntry

--- src/test/java/BibTeXArticleEntryTest.java
+++ src/test/java/BibTeXArticleEntryTest.java
@@ -1,7 +1,16 @@
+import java.io.IOException;
+import java.io.StringWriter;
+
+import org.jabref.logic.bibtex.BibEntryWriter;
+import org.jabref.logic.bibtex.LatexFieldFormatter;
+import org.jabref.logic.util.OS;
 import org.jabref.model.database.BibDatabase;
+import org.jabref.model.database.BibDatabaseMode;
 import org.jabref.model.entry.BibEntry;
+import org.jabref.model.entry.BibEntryTypesManager;
 import org.jabref.model.entry.StandardEntryType;
 import org.jabref.model.entry.field.StandardField;
+import org.jabref.preferences.JabRefPreferences;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -10,9 +19,12 @@ import org.junit.Test;
 public class BibTeXArticleEntryTest {
     private BibDatabase db = new BibDatabase();
     private BibEntry article1;
+    private BibEntryWriter bew;
 
     @Before
     public void setUp() throws Exception {
+        bew = new BibEntryWriter(new LatexFieldFormatter(JabRefPreferences.getInstance().getLatexFieldFormatterPreferences()), new BibEntryTypesManager());
+
         article1 = new BibEntry(StandardEntryType.Article);
         article1.setField(StandardField.AUTHOR, "Marcelo Lubaszewski (UFRGS) and Marcelo Antonio Pavanello (FEI)");
         article1.setField(StandardField.TITLE, "Processes & Materials");
@@ -21,10 +33,26 @@ public class BibTeXArticleEntryTest {
     }
 
     @Test
-    public void articleInsert() {
+    public void _articleInsert() {
         db.insertEntry(article1);
         Assert.assertTrue(db.hasEntries());
         Assert.assertEquals(db.getEntryCount(), 1);
         Assert.assertEquals(db.getEntries().get(0), article1);
+    }
+
+    @Test
+    public void articleCompare() throws IOException {
+        StringWriter sw = new StringWriter();
+
+        bew.write(article1, sw, BibDatabaseMode.BIBTEX);
+        String actual = sw.toString().trim();
+        String expected = "@Article{," + OS.NEWLINE +
+                "  author  = {Marcelo Lubaszewski (UFRGS) and Marcelo Antonio Pavanello (FEI)}," + OS.NEWLINE +
+                "  journal = {Journal of Integrated Circuits and Systems (JICS)}," + OS.NEWLINE +
+                "  title   = {Processes \\& Materials}," + OS.NEWLINE +
+                "  year    = {2019}," + OS.NEWLINE +
+                "}";
+
+        Assert.assertEquals(expected, actual);
     }
 }
